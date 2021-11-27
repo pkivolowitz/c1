2d5b8a624d4775586fdd013efcb57e152b0b9c81
Perry Kivolowitz <perry@kivolowitz.com>
2021-11-26 19:39:13 -0600

    adding to main loop implementation

--- main.cpp
+++ main.cpp
@@ -11,9 +11,17 @@ WINDOW* main_window = nullptr;
 struct sigaction sigIntHandler;
 bool keep_going = true;
 
+enum ACTION {
+	ACTION_NONE,
+	ACTION_QUIT,
+	ACTION_TEST,
+	ACTION_ABOUT
+};
+
 void MainLoop() {
 	while (keep_going) {
 		int c;
+		ACTION action = ACTION_NONE;
 
 		// getch() causes a refresh. This interferes with the erase() in
 		// BeginUpdate().  Therefore, all screen updates begin after the
@@ -22,20 +30,47 @@ void MainLoop() {
 			switch (c) {
 				case 'q':
 				case 'Q':
-					keep_going = false;
+					action = ACTION_QUIT;
+					break;
+
+				case KEY_F(12):
+					action = ACTION_TEST;
 					break;
 			}
 		}
 
-		if (keep_going) {
-			BeginUpdate();
-			Refresh();
-			// Our continuous monitoring of the keyboard causes us to
-			// hog the processor. Calling yield() is a minor kindness
-			// to others.
-			this_thread::yield();
+		// If ACTION_QUIT - leave the main loop.
+		if (action == ACTION_QUIT) {
+			break;
 		}
-	}
+
+		// If NOT ACTION_NONE, begin a screen update.
+		if (action != ACTION_NONE) {
+			BeginUpdate();
+		}
+
+		switch (action) {
+			case ACTION_ABOUT:
+			case ACTION_QUIT:
+			case ACTION_NONE:
+				// NOP.
+				break;
+
+			case ACTION_TEST:
+				mvaddch(10, 10, '*');
+				break;
+		}
+
+		// If NOT ACTION_NONE, complete the screen update.
+		if (action != ACTION_NONE) {
+			Refresh();
+		}
+
+		// Our continuous monitoring of the keyboard causes us to
+		// hog the processor. Calling yield() is a minor kindness
+		// to others.
+		this_thread::yield();
+		}
 }
 
 int main(int argc, char** argv) {
